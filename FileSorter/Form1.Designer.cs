2d77b9b72451b9a37d2ee124f3e233b1c7293b6e
Warren Garabrandt <wmgarab@hotmail.com>
2024-11-06 16:27:46 -0600

    Add project files.

---
+++ FileSorter/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace FileSorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.cmdBrowseSource = new System.Windows.Forms.Button();
+            this.chklstFileTypes = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmdScan = new System.Windows.Forms.Button();
+            this.txtFileList = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.cmdBrowseOutput = new System.Windows.Forms.Button();
+            this.cmdProcess = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblFileCount = new System.Windows.Forms.Label();
+            this.lblSelectedCount = new System.Windows.Forms.Label();
+            this.lblSelectedSize = new System.Windows.Forms.Label();
+            this.chkCheckAllTypes = new System.Windows.Forms.Button();
+            this.cmdUncheckAllTypes = new System.Windows.Forms.Button();
+            this.cmdCheckCommonTypes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source Directory";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSource.Location = new System.Drawing.Point(12, 25);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(614, 20);
+            this.txtSource.TabIndex = 1;
+            // 
+            // cmdBrowseSource
+            // 
+            this.cmdBrowseSource.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdBrowseSource.Location = new System.Drawing.Point(632, 23);
+            this.cmdBrowseSource.Name = "cmdBrowseSource";
+            this.cmdBrowseSource.Size = new System.Drawing.Size(75, 23);
+            this.cmdBrowseSource.TabIndex = 2;
+            this.cmdBrowseSource.Text = "Browse";
+            this.cmdBrowseSource.UseVisualStyleBackColor = true;
+            this.cmdBrowseSource.Click += new System.EventHandler(this.cmdBrowseSource_Click);
+            // 
+            // chklstFileTypes
+            // 
+            this.chklstFileTypes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.chklstFileTypes.FormattingEnabled = true;
+            this.chklstFileTypes.IntegralHeight = false;
+            this.chklstFileTypes.Location = new System.Drawing.Point(12, 70);
+            this.chklstFileTypes.Name = "chklstFileTypes";
+            this.chklstFileTypes.Size = new System.Drawing.Size(171, 275);
+            this.chklstFileTypes.TabIndex = 3;
+            this.chklstFileTypes.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.chklstFileTypes_ItemCheck);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Found File Types";
+            // 
+            // cmdScan
+            // 
+            this.cmdScan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdScan.Location = new System.Drawing.Point(713, 23);
+            this.cmdScan.Name = "cmdScan";
+            this.cmdScan.Size = new System.Drawing.Size(75, 23);
+            this.cmdScan.TabIndex = 5;
+            this.cmdScan.Text = "Scan";
+            this.cmdScan.UseVisualStyleBackColor = true;
+            this.cmdScan.Click += new System.EventHandler(this.cmdScan_Click);
+            // 
+            // txtFileList
+            // 
+            this.txtFileList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFileList.Location = new System.Drawing.Point(189, 70);
+            this.txtFileList.Multiline = true;
+            this.txtFileList.Name = "txtFileList";
+            this.txtFileList.Size = new System.Drawing.Size(599, 304);
+            this.txtFileList.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 377);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Output Directory (make sure it is empty!)";
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDestination.Location = new System.Drawing.Point(12, 393);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(614, 20);
+            this.txtDestination.TabIndex = 8;
+            // 
+            // cmdBrowseOutput
+            // 
+            this.cmdBrowseOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdBrowseOutput.Location = new System.Drawing.Point(632, 391);
+            this.cmdBrowseOutput.Name = "cmdBrowseOutput";
+            this.cmdBrowseOutput.Size = new System.Drawing.Size(75, 23);
+            this.cmdBrowseOutput.TabIndex = 9;
+            this.cmdBrowseOutput.Text = "Browse";
+            this.cmdBrowseOutput.UseVisualStyleBackColor = true;
+            this.cmdBrowseOutput.Click += new System.EventHandler(this.cmdBrowseOutput_Click);
+            // 
+            // cmdProcess
+            // 
+            this.cmdProcess.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdProcess.Location = new System.Drawing.Point(713, 391);
+            this.cmdProcess.Name = "cmdProcess";
+            this.cmdProcess.Size = new System.Drawing.Size(75, 23);
+            this.cmdProcess.TabIndex = 10;
+            this.cmdProcess.Text = "Process";
+            this.cmdProcess.UseVisualStyleBackColor = true;
+            this.cmdProcess.Click += new System.EventHandler(this.cmdProcess_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 419);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // lblFileCount
+            // 
+            this.lblFileCount.AutoSize = true;
+            this.lblFileCount.Location = new System.Drawing.Point(186, 54);
+            this.lblFileCount.Name = "lblFileCount";
+            this.lblFileCount.Size = new System.Drawing.Size(58, 13);
+            this.lblFileCount.TabIndex = 12;
+            this.lblFileCount.Text = "Total Files:";
+            // 
+            // lblSelectedCount
+            // 
+            this.lblSelectedCount.AutoSize = true;
+            this.lblSelectedCount.Location = new System.Drawing.Point(375, 54);
+            this.lblSelectedCount.Name = "lblSelectedCount";
+            this.lblSelectedCount.Size = new System.Drawing.Size(76, 13);
+            this.lblSelectedCount.TabIndex = 13;
+            this.lblSelectedCount.Text = "Selected Files:";
+            // 
+            // lblSelectedSize
+            // 
+            this.lblSelectedSize.AutoSize = true;
+            this.lblSelectedSize.Location = new System.Drawing.Point(591, 54);
+            this.lblSelectedSize.Name = "lblSelectedSize";
+            this.lblSelectedSize.Size = new System.Drawing.Size(75, 13);
+            this.lblSelectedSize.TabIndex = 14;
+            this.lblSelectedSize.Text = "Selected Size:";
+            // 
+            // chkCheckAllTypes
+            // 
+            this.chkCheckAllTypes.Location = new System.Drawing.Point(12, 351);
+            this.chkCheckAllTypes.Name = "chkCheckAllTypes";
+            this.chkCheckAllTypes.Size = new System.Drawing.Size(41, 23);
+            this.chkCheckAllTypes.TabIndex = 15;
+            this.chkCheckAllTypes.Text = "All";
+            this.chkCheckAllTypes.UseVisualStyleBackColor = true;
+            this.chkCheckAllTypes.Click += new System.EventHandler(this.chkCheckAllTypes_Click);
+            // 
+            // cmdUncheckAllTypes
+            // 
+            this.cmdUncheckAllTypes.Location = new System.Drawing.Point(59, 351);
+            this.cmdUncheckAllTypes.Name = "cmdUncheckAllTypes";
+            this.cmdUncheckAllTypes.Size = new System.Drawing.Size(52, 23);
+            this.cmdUncheckAllTypes.TabIndex = 16;
+            this.cmdUncheckAllTypes.Text = "None";
+            this.cmdUncheckAllTypes.UseVisualStyleBackColor = true;
+            this.cmdUncheckAllTypes.Click += new System.EventHandler(this.cmdUncheckAllTypes_Click);
+            // 
+            // cmdCheckCommonTypes
+            // 
+            this.cmdCheckCommonTypes.Location = new System.Drawing.Point(117, 351);
+            this.cmdCheckCommonTypes.Name = "cmdCheckCommonTypes";
+            this.cmdCheckCommonTypes.Size = new System.Drawing.Size(66, 23);
+            this.cmdCheckCommonTypes.TabIndex = 17;
+            this.cmdCheckCommonTypes.Text = "Common";
+            this.cmdCheckCommonTypes.UseVisualStyleBackColor = true;
+            this.cmdCheckCommonTypes.Click += new System.EventHandler(this.cmdCheckCommonTypes_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmdCheckCommonTypes);
+            this.Controls.Add(this.cmdUncheckAllTypes);
+            this.Controls.Add(this.chkCheckAllTypes);
+            this.Controls.Add(this.lblSelectedSize);
+            this.Controls.Add(this.lblSelectedCount);
+            this.Controls.Add(this.lblFileCount);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.cmdProcess);
+            this.Controls.Add(this.cmdBrowseOutput);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFileList);
+            this.Controls.Add(this.cmdScan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chklstFileTypes);
+            this.Controls.Add(this.cmdBrowseSource);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Sorter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button cmdBrowseSource;
+        private System.Windows.Forms.CheckedListBox chklstFileTypes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cmdScan;
+        private System.Windows.Forms.TextBox txtFileList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Button cmdBrowseOutput;
+        private System.Windows.Forms.Button cmdProcess;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblFileCount;
+        private System.Windows.Forms.Label lblSelectedCount;
+        private System.Windows.Forms.Label lblSelectedSize;
+        private System.Windows.Forms.Button chkCheckAllTypes;
+        private System.Windows.Forms.Button cmdUncheckAllTypes;
+        private System.Windows.Forms.Button cmdCheckCommonTypes;
+    }
+}
+
---
+++ FileSorter/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSorter
+{
+    public partial class Form1 : Form
+    {
+
+        private string[] CommonTypes = new string[]
+        {
+            ".doc", ".docm", ".docx", ".dot", ".dotm", ".dotx",
+            ".xls", ".xlsx", ".xlsm", ".csv", ".xla", ".xlam", ".xlsb", ".xlt", ".xltm",
+            ".ppt", ".pptx", ".pot", ".potm", ".ppa", ".ppam", ".pps", ".ppsm", ".ppsx", 
+            ".pub", ".vsd", ".vsdx", ".vdwm", ".dia", ".drawio", ".ai",
+            ".mpp", ".mpx", ".mspdi",
+            ".odt", ".fodt", ".ods", ".fods", ".odp", ".fodp", ".odg", ".fodg", ".odf", ".sda", ".sdb", ".sdc", ".sdd",
+            ".rtf", ".txt", ".htm", ".html", ".wps", ".mht", ".mhtml", ".xml", ".prn", ".slk", ".xaml", ".js", ".css",
+            ".xps", ".oxps", ".pdf",
+            ".bmp", ".emf",".gif",".jpg",".jpeg",".png",".tif",".tiff",".tga",".eps",".webp",".svg",
+            ".epub", ".mobi", 
+            ".eml", ".msg"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdBrowseSource_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dlg = new FolderBrowserDialog())
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    txtSource.Text = dlg.SelectedPath;
+                }
+            }
+        }
+
+        private bool Updating = false;
+
+        private void cmdScan_Click(object sender, EventArgs e)
+        {
+            DirIndex.Clear();
+            DirList.Clear();
+            FileList.Clear();
+            ExtensionIndex.Clear();
+            GC.Collect();
+            WalkDirectory(txtSource.Text, null);
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            Updating = true;
+            List<string> checkedItems = new List<string>();
+            foreach (var item in chklstFileTypes.CheckedItems)
+            {
+                checkedItems.Add(item.ToString());
+            }
+            chklstFileTypes.Items.Clear();
+            foreach (var item in ExtensionIndex)
+            {
+                chklstFileTypes.Items.Add(item.Key);
+            }
+            foreach (var item in checkedItems)
+            {
+                int index = chklstFileTypes.Items.IndexOf(item.ToString());
+                if (index != -1)
+                {
+                    chklstFileTypes.SetItemChecked(index, true);
+                }
+            }
+            RefreshCounters();
+            
+            Updating = false;
+        }
+
+        private void RefreshCounters()
+        {
+            lblFileCount.Text = string.Format("Total Files: {0}", FileList.Count());
+            long fileCount = FileList.Where(x => x.Selected).Count();
+            long fileSize = FileList.Where(x => x.Selected).Sum(y => y.Size);
+            lblSelectedCount.Text = string.Format("Selected Files: {0}", fileCount);
+            lblSelectedSize.Text = string.Format("Selected Size: {0}", PrettyFormat(fileSize));
+        }
+
+        private string PrettyFormat(long size)
+        {
+            if (Math.Abs(size) >= 1000000000000)
+            {
+                return string.Format("{0:0.00} TB", size / 1099511627776);
+            }
+            else if (Math.Abs(size) >= 1000000000)
+            {
+                return string.Format("{0:0.00} GB", size / 1073741824);
+            }
+            else if (Math.Abs(size) >= 1000000)
+            {
+                return string.Format("{0:0.00} MB", size / 1048576);
+            }
+            else if (Math.Abs(size) >= 1000)
+            {
+                return string.Format("{0:0.00} KB", size / 1024);
+            }
+            else
+            {
+                return string.Format("{0} bytes", size);
+            }
+        }
+
+        public Dictionary<string, DirItem> DirIndex = new Dictionary<string, DirItem>();
+        public List<DirItem> DirList = new List<DirItem>();
+        public List<FileItem> FileList = new List<FileItem>();
+        public Dictionary<string, List<FileItem>> ExtensionIndex = new Dictionary<string, List<FileItem>>();
+
+        public class FileItem
+        {
+            public string FileName;
+            public string Extension;
+            public DirItem Parent;
+            public long Size = 0;
+            public bool Selected = false;
+        }
+
+        public class DirItem
+        {
+            public string PartialPath;
+            public string AbsolutePath;
+            public DirItem Parent;
+            public List<DirItem> ChildDirs = new List<DirItem>();
+            public List<FileItem> ChildFiles = new List<FileItem>();
+        }
+        
+        private void WalkDirectory(string path, DirItem parent)
+        {
+            try
+            {
+                if (parent == null)
+                {
+                    parent = new DirItem()
+                    {
+                        AbsolutePath = path,
+                        Parent = null,
+                        ChildDirs = new List<DirItem>(),
+                        ChildFiles = new List<FileItem>(),
+                        PartialPath = ""
+                    };
+                    DirList.Add(parent);
+                }
+                try
+                {
+                    string[] files = Directory.GetFiles(path);
+                    foreach (var file in files)
+                    {
+                        try
+                        {
+                            FileInfo finfo = new FileInfo(file);
+                            var newFileItem = new FileItem()
+                            {
+                                Extension = finfo.Extension,
+                                Parent = parent,
+                                FileName = finfo.Name,
+                                Selected = false,
+                                Size = finfo.Length
+                            };
+                            parent.ChildFiles.Add(newFileItem);
+                            FileList.Add(newFileItem);
+
+                            List<FileItem> extList = null;
+                            if (!ExtensionIndex.TryGetValue(finfo.Extension, out extList))
+                            {
+                                extList = new List<FileItem>();
+                                ExtensionIndex.Add(finfo.Extension, extList);
+                            }
+                            extList.Add(newFileItem);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                try
+                {
+                    string[] dirs = Directory.GetDirectories(path);
+                    foreach (var dir in dirs) 
+                    {
+                        DirItem newDirItem = new DirItem()
+                        {
+                            AbsolutePath = dir,
+                            ChildDirs = new List<DirItem>(),
+                            ChildFiles = new List<FileItem>(),
+                            Parent = parent,
+                            PartialPath = Path.GetDirectoryName(dir)
+                        };
+                        parent.ChildDirs.Add(newDirItem);
+                        WalkDirectory(dir, newDirItem);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void chklstFileTypes_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (Updating)
+            {
+                return;
+            }
+            CheckState newVal = e.NewValue;
+            bool NewBoolValue = newVal == CheckState.Checked;
+            List<FileItem> ExtFiles;
+            if (ExtensionIndex.TryGetValue(chklstFileTypes.Items[e.Index].ToString(), out ExtFiles))
+            {
+                foreach (var f in ExtFiles)
+                {
+                    f.Selected = NewBoolValue;
+                }
+            }
+            RefreshCounters();
+        }
+
+        private void cmdBrowseOutput_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dlg = new FolderBrowserDialog())
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    txtDestination.Text = dlg.SelectedPath;
+                }
+            }
+        }
+
+        private void cmdProcess_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(txtSource.Text))
+            {
+                MessageBox.Show("Select a source directory that exists.");
+                return;
+            }
+            if (DirList.Count == 0 || FileList.Count == 0)
+            {
+                MessageBox.Show("No files to process. Run a scan and check at least one file type.");
+                return;
+            }
+            if (!Directory.Exists(txtDestination.Text))
+            {
+                MessageBox.Show("Select a destination directory.");
+                return;
+            }
+            if (Directory.GetFiles(txtDestination.Text).Any() || Directory.GetDirectories(txtDestination.Text).Any())
+            {
+                MessageBox.Show("Make sure the destination directory is empty.");
+                return;
+            }
+            if (!FileList.Where(x => x.Selected).Any())
+            {
+                MessageBox.Show("No files selected.");
+                return;
+            }
+            RunProcess(txtDestination.Text);
+        }
+
+        private void cmdUncheckAllTypes_Click(object sender, EventArgs e)
+        {
+            Updating = true;
+            for (int i = 0; i < chklstFileTypes.Items.Count; i++)
+            {
+                chklstFileTypes.SetItemChecked(i, false);
+            }
+            foreach (var f in FileList)
+            {
+                f.Selected = false;
+            }
+            Updating = false;
+            RefreshCounters();
+        }
+
+        private void chkCheckAllTypes_Click(object sender, EventArgs e)
+        {
+            Updating = true;
+            for (int i = 0; i < chklstFileTypes.Items.Count; i++)
+            {
+                chklstFileTypes.SetItemChecked(i, true);
+            }
+            foreach (var f in FileList)
+            {
+                f.Selected = true;
+            }
+            Updating = false;
+            RefreshCounters();
+        }
+
+        private void cmdCheckCommonTypes_Click(object sender, EventArgs e)
+        {
+            foreach (var f in FileList)
+            {
+                if (CommonTypes.Contains(f.Extension, StringComparer.OrdinalIgnoreCase))
+                {
+                    f.Selected = true;
+                }
+                else
+                {
+                    f.Selected = false;
+                }
+            }
+            Updating = true;
+            for (int i = 0; i < chklstFileTypes.Items.Count; i++)
+            {
+                if (CommonTypes.Contains(chklstFileTypes.Items[i].ToString(), StringComparer.OrdinalIgnoreCase))
+                {
+                    chklstFileTypes.SetItemChecked(i, true);
+                }
+                else
+                {
+                    chklstFileTypes.SetItemChecked(i, false);
+
+                }
+            }
+            Updating = false;
+            RefreshCounters();
+        }
+        private void RunProcess(string destination)
+        {
+
+        }
+    }
+}
